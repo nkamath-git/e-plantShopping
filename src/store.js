9e49a31d87766816b052ebd7d6673998cd1b5b58
Nikhil Kamath <nikhilkamath@hotmail.com>
2026-06-12 13:00:29 -0400

    Configure Vite GitHub Pages deployment

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
+
+// Create Redux store
+const store = configureStore({
     reducer: {
         cart: cartReducer,
     },
 });
-export default store
+
+export default store;
